01519f91c5e35f3795843de336789a57b7272f7e
Hrishikesh Paul <hrishikesh.paul.2763@gmail.com>
2025-06-26 00:05:42 +0900

    Make the language grid responsive for different screen widths and add chart

--- languages/script.js
+++ languages/script.js
@@ -2,6 +2,8 @@ gsap.defaults({
     ease: "power2.out"
 });
 
+// ========================================================
+// Animations for the language grid items.
 const languageButtons = document.querySelectorAll(".language-grid-item");
 let buttonTimelines = [];
 let exitTime;
@@ -10,9 +12,14 @@ for (let languageButton of languageButtons) {
     let tl = gsap.timeline({
         paused: true
     })
+    .set(languageButton, {
+        zIndex: 98  // Move the button to the front
+    })
     .to(languageButton, {
         scale: 1.2,
         filter: "brightness(1)",
+        outline: "7px solid white",
+        boxShadow: "0 0 20px 5px white",
         duration: 0.3
     })  // Entrance animation
     .addPause();
@@ -20,8 +27,13 @@ for (let languageButton of languageButtons) {
     tl.to(languageButton, {
         scale: 1,
         filter: "brightness(0.5)",
+        outline: "0px solid white",
+        boxShadow: "0 0 0px 0px white",
         duration: 0.2
-    });  // Exit animation
+    })  // Exit animation
+    .set(languageButton, {
+        zIndex: 0  // Move the button to the back when the animation is over.
+    });
     buttonTimelines.push(tl);
 }
 
@@ -47,6 +59,7 @@ for (let i = 0; i < languageButtons.length; i++) {
     addHoverAnimation(buttonTimelines[i], exitTime, languageButtons[i]);
 }
 
+// ========================================================
 // Heading animation. Increases the font size of the headings on hover.
 const headingSpans = document.querySelectorAll("#section-1 span");
 for (let headingSpan of headingSpans) {
@@ -54,17 +67,18 @@ for (let headingSpan of headingSpans) {
         paused: true
     })
     .to(headingSpan, {
-        fontSize: "6rem",
+        fontSize: "6vw",
         duration: 0.3
     })
     .addPause()
     .to(headingSpan, {
-        fontSize: "5rem",
+        fontSize: "5vw",
         duration: 0.3
     });
     addHoverAnimation(fontSizeAnim, 0.3, headingSpan);
 }
 
+// =======================================================
 // Animations for .language-section.
 const languageSectionThemes = {
     "#python-section": {
@@ -120,4 +134,27 @@ for (const idName in languageSectionThemes) {
         scaleX: 0
     });
     addHoverAnimation(getLanguageSectionAnimation(idName), 0.3, document.querySelector(idName));
+}
+
+
+// =======================================================
+// Animations for the chart columns.
+const chartColumns = document.querySelectorAll("#table td");
+for (let chartColumn of chartColumns) {
+    let bgColor = chartColumn.style["background-color"];
+    let chartColumnAnim = gsap.timeline({
+        paused: true
+    })
+    .to(chartColumn, {
+        backgroundColor: "var(--main-color-dark)",
+        outline: "5px solid white",
+        duration: 0.3
+    })
+    .addPause()
+    .to(chartColumn, {
+        backgroundColor: bgColor,
+        outline: "0px solid white",
+        duration: 0.3
+    });
+    addHoverAnimation(chartColumnAnim, 0.3, chartColumn);
 }
